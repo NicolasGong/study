06f8211c4d8ce0b5190f34c572439db57a31623a
YiningGong <nicolasgongyn@gmail.com>
2017-11-17 22:46:01 +0800

    提交新的测试代码

--- littlecase/src/com/gyn/home/HelloGitHub.java
+++ littlecase/src/com/gyn/home/HelloGitHub.java
@@ -3,5 +3,6 @@ package com.gyn.home;
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("Haha ,github I am coming! Let me try!");
+        System.out.println("A ha ,it's very easy to use,i like it!");
     }
 }

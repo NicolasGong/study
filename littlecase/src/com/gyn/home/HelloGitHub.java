5a15eda8581db37db6984af92e0db06e945e75e3
nicolasgong <nicolasgongyn@gmail.com>
2021-05-08 14:54:26 +0800

    测试提交3

--- littlecase/src/com/gyn/home/HelloGitHub.java
+++ littlecase/src/com/gyn/home/HelloGitHub.java
@@ -5,5 +5,6 @@ public class HelloGitHub {
         System.out.println("Haha ,github I am coming! Let me try!");
         System.out.println("A ha ,it's very easy to use,i like it!");
         System.out.println("brach-0.1");
+        System.out.println("brach-0.2");
     }
 }

50877d399714d045f442f4498b2981b08d47b745
nicolasgong <nicolasgongyn@gmail.com>
2021-05-08 17:04:16 +0800

    commit test

--- jicheng/src/com/gyn/home/TestMain.java
+++ jicheng/src/com/gyn/home/TestMain.java
@@ -4,5 +4,6 @@ public class TestMain {
     public static void main(String[] args) {
         DataRegionScanner scanner = new DataRegionScanner();
         scanner.scanRegion();
+        System.out.println("brach-0.3");
     }
 }
--- littlecase/src/com/gyn/home/HelloGitHub.java
+++ littlecase/src/com/gyn/home/HelloGitHub.java
@@ -6,5 +6,6 @@ public class HelloGitHub {
         System.out.println("A ha ,it's very easy to use,i like it!");
         System.out.println("brach-0.1");
         System.out.println("brach-0.2");
+        System.out.println("brach-0.3");
     }
 }
